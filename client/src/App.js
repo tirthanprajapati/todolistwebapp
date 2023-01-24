7c5f947c6ff17886ce49c46733a5dc75ae4adeb8
Tirthan Prajapati <82008300+tirthanprajapati@users.noreply.github.com>
2023-01-24 13:12:52 +0530

    v1.0.0 beta

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="head">Todo List</div>
+      <div className="box" >
+        <div className='navbar'>
+          <a href="#home">All Todo's</a>
+          <a href="#news">Active Todo's</a>
+          <a href="#contact">Done Todo's</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
